592b9504216b476198866b9646eb8033bca1db6c
juan luis <reejulu@gmail.com>
2019-05-17 09:02:13 +0200

    version 01 con paso del objeto view desde el linearlayout

--- app/src/main/java/com/example/milinear/EjercicioPresionarColores.java
+++ app/src/main/java/com/example/milinear/EjercicioPresionarColores.java
@@ -7,14 +7,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 import static com.example.milinear.R.layout.activity_ejercicio_presionar_colores;
 
 public class EjercicioPresionarColores extends AppCompatActivity {
     private LinearLayout padre;
+    long tInicio;
+    long tInicioBoton;
     private int ncolor;
+    Button boton;
     private int num_veces ;
+    boolean inicio;
     boolean bloque1click;
     boolean bloque2click;
     boolean bloque3click;
@@ -26,6 +31,8 @@ public class EjercicioPresionarColores extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(activity_ejercicio_presionar_colores);
         this.ncolor = getResources().getColor(R.color.negro);
+        this.tInicio = System.currentTimeMillis();
+        boton = findViewById(R.id.boton_iniciar);
     }
 
     public void marcarColor1(View view){
@@ -38,7 +45,22 @@ public class EjercicioPresionarColores extends AppCompatActivity {
 
     }
 
-    private void salir(){
+    public void iniciar (View view){
+        this.tInicioBoton = System.currentTimeMillis();
+        boton.setVisibility(View.INVISIBLE);
+        marcarColor(view);
+        inicio = true;
+
+    }
+
+    public void salir(){
+        long tFinal = System.currentTimeMillis();
+        double tDuracion = (tFinal-tInicio)/1000d;
+        double tDuracion1 = (tFinal-tInicioBoton)/1000d;
+        String Texto = String.format("Duracion en segundos de la App es: %1$.3f",tDuracion);
+        String Texto1 = String.format("Duracion en segundos de la App es: %1$.3f",tDuracion1);
+        Log.i("test",Texto);
+        Log.i("test",Texto1);
         this.finish();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
             this.finishAffinity();
@@ -47,7 +69,7 @@ public class EjercicioPresionarColores extends AppCompatActivity {
 
     public void marcarColor(View view) {
         final LinearLayout pepe =(LinearLayout)findViewById(R.id.bloque1);
-
+    if (inicio == true) {
 
         pepe.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,21 +78,21 @@ public class EjercicioPresionarColores extends AppCompatActivity {
                 if (bloque1click == false) {
                     pepe.setBackgroundColor(getResources().getColor(R.color.negro));
                     bloque1click = true;
-                    num_veces = num_veces +1;
-                    Log.i("test","numero de veces " +num_veces);
-                    if (num_veces == 6){
+                    num_veces = num_veces + 1;
+                    Log.i("test", "numero de veces " + num_veces);
+                    if (num_veces == 6) {
                         salir();
                     }
                 } else {
                     pepe.setBackgroundColor(getResources().getColor(R.color.blanco));
                     bloque1click = false;
-                    num_veces = num_veces -1;
-                    Log.i("test","numero de veces " +num_veces);
+                    num_veces = num_veces - 1;
+                    Log.i("test", "numero de veces " + num_veces);
                 }
             }
         });
 
-        final LinearLayout pepe2 =(LinearLayout)findViewById(R.id.bloque2);
+        final LinearLayout pepe2 = (LinearLayout) findViewById(R.id.bloque2);
         pepe2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -78,21 +100,21 @@ public class EjercicioPresionarColores extends AppCompatActivity {
                 if (bloque2click == false) {
                     pepe2.setBackgroundColor(getResources().getColor(R.color.negro));
                     bloque2click = true;
-                    num_veces = num_veces +1;
-                    Log.i("test","numero de veces " +num_veces);
-                    if (num_veces == 6){
+                    num_veces = num_veces + 1;
+                    Log.i("test", "numero de veces " + num_veces);
+                    if (num_veces == 6) {
                         salir();
                     }
                 } else {
                     pepe2.setBackgroundColor(getResources().getColor(R.color.blanco));
                     bloque2click = false;
-                    num_veces = num_veces -1;
-                    Log.i("test","numero de veces " +num_veces);
+                    num_veces = num_veces - 1;
+                    Log.i("test", "numero de veces " + num_veces);
                 }
             }
         });
 
-        final LinearLayout pepe3 =(LinearLayout)findViewById(R.id.bloque3);
+        final LinearLayout pepe3 = (LinearLayout) findViewById(R.id.bloque3);
         pepe3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -100,20 +122,20 @@ public class EjercicioPresionarColores extends AppCompatActivity {
                 if (bloque3click == false) {
                     pepe3.setBackgroundColor(getResources().getColor(R.color.negro));
                     bloque3click = true;
-                    num_veces = num_veces +1;
-                    Log.i("test","numero de veces " +num_veces);
-                    if (num_veces == 6){
+                    num_veces = num_veces + 1;
+                    Log.i("test", "numero de veces " + num_veces);
+                    if (num_veces == 6) {
                         salir();
                     }
                 } else {
                     pepe3.setBackgroundColor(getResources().getColor(R.color.blanco));
                     bloque3click = false;
-                    num_veces = num_veces -1;
-                    Log.i("test","numero de veces " +num_veces);
+                    num_veces = num_veces - 1;
+                    Log.i("test", "numero de veces " + num_veces);
                 }
             }
         });
-        final LinearLayout pepe4 =(LinearLayout)findViewById(R.id.bloque4);
+        final LinearLayout pepe4 = (LinearLayout) findViewById(R.id.bloque4);
         pepe4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -121,20 +143,20 @@ public class EjercicioPresionarColores extends AppCompatActivity {
                 if (bloque4click == false) {
                     pepe4.setBackgroundColor(getResources().getColor(R.color.negro));
                     bloque4click = true;
-                    num_veces = num_veces +1;
-                    Log.i("test","numero de veces " +num_veces);
-                    if (num_veces == 6){
+                    num_veces = num_veces + 1;
+                    Log.i("test", "numero de veces " + num_veces);
+                    if (num_veces == 6) {
                         salir();
                     }
                 } else {
                     pepe4.setBackgroundColor(getResources().getColor(R.color.blanco));
                     bloque4click = false;
-                    num_veces = num_veces -1;
-                    Log.i("test","numero de veces " +num_veces);
+                    num_veces = num_veces - 1;
+                    Log.i("test", "numero de veces " + num_veces);
                 }
             }
         });
-        final LinearLayout pepe5 =(LinearLayout)findViewById(R.id.bloque5);
+        final LinearLayout pepe5 = (LinearLayout) findViewById(R.id.bloque5);
         pepe5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -142,20 +164,20 @@ public class EjercicioPresionarColores extends AppCompatActivity {
                 if (bloque5click == false) {
                     pepe5.setBackgroundColor(getResources().getColor(R.color.negro));
                     bloque5click = true;
-                    num_veces = num_veces +1;
-                    Log.i("test","numero de veces " +num_veces);
-                    if (num_veces == 6){
+                    num_veces = num_veces + 1;
+                    Log.i("test", "numero de veces " + num_veces);
+                    if (num_veces == 6) {
                         salir();
                     }
                 } else {
                     pepe5.setBackgroundColor(getResources().getColor(R.color.blanco));
                     bloque5click = false;
-                    num_veces = num_veces -1;
-                    Log.i("test","numero de veces " +num_veces);
+                    num_veces = num_veces - 1;
+                    Log.i("test", "numero de veces " + num_veces);
                 }
             }
         });
-        final LinearLayout pepe6 =(LinearLayout)findViewById(R.id.bloque6);
+        final LinearLayout pepe6 = (LinearLayout) findViewById(R.id.bloque6);
         pepe6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -163,24 +185,26 @@ public class EjercicioPresionarColores extends AppCompatActivity {
                 if (bloque6click == false) {
                     pepe6.setBackgroundColor(getResources().getColor(R.color.negro));
                     bloque6click = true;
-                    num_veces = num_veces +1;
-                    Log.i("test","numero de veces " +num_veces);
-                    if (num_veces == 6){
+                    num_veces = num_veces + 1;
+                    Log.i("test", "numero de veces " + num_veces);
+                    if (num_veces == 6) {
                         salir();
                     }
                 } else {
                     pepe6.setBackgroundColor(getResources().getColor(R.color.blanco));
                     bloque6click = false;
-                    num_veces = num_veces -1;
-                    Log.i("test","numero de veces " +num_veces);
+                    num_veces = num_veces - 1;
+                    Log.i("test", "numero de veces " + num_veces);
                 }
             }
         });
-
+    }
        // padre = (LinearLayout) getResources().getLayout(activity_ejercicio_presionar_colores);;
     //padre = (LinearLayout) findViewById(R.id.bloque).getResources().getLayout(activity_ejercicio_presionar_colores);
    // Log.i("hola","padre es : " + (padre.getResources().getLayout(activity_ejercicio_presionar_colores)));
     //padre.setBackgroundColor(getResources().getColor(R.color.azul));
 
     }
+
+
 }
---
+++ app/src/main/java/com/example/milinear/EjercicioPresionarColoresVersion1.java
@@ -0,0 +1,145 @@
+package com.example.milinear;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+
+import static com.example.milinear.R.layout.activity_ejercicio_presionar_colores;
+
+// VERSION 1
+// ESTA SE BASA EN PASAR EL VIEW view del XML para pasar la accion del OnClick
+//  En layout.xml
+//        Button ------> OnClick ---> al metodo "iniciar"
+//  En file.java
+//  LinearLayout cajatocada = (LinearLayout)view;   --->donde la vista buton se copia a cajatocada
+//  Para empezar a contar el tiempo de duraccion hasta que se cambian todas las casillas se crea
+//  un boolean que esta a false hasta que el boton INICIAR se ha pulsado .
+//            -En este momento el boton se hace invisible y el boolean se pone true
+//  para identificar el LinearLayout presionado hay que leer el getId de la view y guardarlo en
+//  un array: -Si ya estaba en el array hay que quitarlo y decrementar el numero de casillas cambiadas
+//            -Si no lo estaba se añade al array
+//            -Si no hay ninguno simplemente se añade
+// Cuando el numero de casillas cambiada es 5 entonces se cierra la App.
+
+
+public class EjercicioPresionarColoresVersion1 extends AppCompatActivity {
+
+    long tInicio;
+    long tInicioBoton;
+    Button boton;
+    private int num_veces ;
+    boolean inicio;
+    LinearLayout id;
+    Button cajatocada;
+    Button finalizar;
+    ArrayList<Integer> ids = new ArrayList<Integer>();
+
+    boolean bloqueclick;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_ejercicio_presionar_colores);
+        this.tInicio = System.currentTimeMillis();
+        boton = findViewById(R.id.boton_iniciar);
+
+    }
+
+    public void iniciar (View view){
+        cajatocada = (Button) view;
+        // al presionar el boton se mira el tiempo, se pone invisible el boton y se inicia el marcado
+        //  de las cajas, ademas se habilita el mismo poniendo inicio = true
+        this.tInicioBoton = System.currentTimeMillis();
+        boton.setVisibility(View.INVISIBLE);
+        //finalizar.setVisibility(View.INVISIBLE);
+        inicio = true;
+    }
+    public void finalizarsi (View view){
+        finalizar = (Button) view;
+        salir();
+    }
+
+    public void marcarColor(View view) {
+        // Si ya se ha pulsado el Boton Inicio entonces --true
+        // eso quiere decir que empezamos a poder cambiar cajas de colores
+
+        //id = view.findViewById(R.id.bloque1);
+        LinearLayout x = (LinearLayout) view;
+        if (inicio == true) {
+        if (ids.size() == 0){
+            ids.add(x.getId());
+            num_veces = num_veces +1;
+            bloqueclick = false;    // poner en negro
+        }else {
+            int temporal = num_veces;
+            for (int i=0 ;i<= ids.size()-1;i++){
+                if (ids.get(i)== x.getId()){
+                    num_veces = num_veces - 1;
+                    ids.remove(i);
+                    bloqueclick = true;  // poner en blanco
+                    break;
+                }
+            }
+            if (temporal == num_veces){
+                ids.add(x.getId());
+                num_veces = num_veces +1;
+                bloqueclick = false;    // poner en negro
+            }else {
+                //no hay que hacer nada ya estaba y se ha quitado
+            }
+        }
+        Log.i("test","array momentaneo es: " + ids);
+        Log.i("test","el linear pulsado es " +x.getId());
+       // Log.i("test","x" +x.toString().contains("bloque"));
+
+      //  if (inicio == true) {
+            if (bloqueclick == false) {
+                view.setBackgroundColor(getResources().getColor(R.color.negro));
+                bloqueclick = true;
+                Log.i("test", "numero de veces " + num_veces);
+                if (num_veces == 6) {
+                    continuar(cajatocada);
+                    //salir();
+                }
+            } else {
+                view.setBackgroundColor(getResources().getColor(R.color.blanco));
+                bloqueclick = false;
+                Log.i("test", "numero de veces " + num_veces);
+            }
+
+            }else {
+            // No hay que hacer nada aun . Esperar a que el boton INICIAR se presione
+        }
+    }
+
+    public void continuar(Button cajatocada){
+        Button x = (Button) cajatocada;
+        x.setVisibility(View.VISIBLE);
+        x.append(" continuar? si",0,14);
+        inicio = false;
+       // finalizar.setVisibility(View.VISIBLE);
+    }
+
+    public void salir(){
+        long tFinal = System.currentTimeMillis();
+        double tDuracion = (tFinal-tInicio)/1000d;
+        double tDuracion1 = (tFinal-tInicioBoton)/1000d;
+        String Texto = String.format("Duracion en segundos de la App es: %1$.3f",tDuracion);
+        String Texto1 = String.format("Duracion en segundos de la App es: %1$.3f",tDuracion1);
+        Log.i("test",Texto);
+        Log.i("test",Texto1);
+        this.finish();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            this.finishAffinity();
+        }
+    }
+
+
+}
